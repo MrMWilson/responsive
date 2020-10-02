9ab9a12c0fc8814b36536205350b8a5142478410
Mike Wilson <mrmwilson@live.co.uk>
2020-10-02 19:37:47 +0100

    added update text

--- otherPages/storage/storage.js
+++ otherPages/storage/storage.js
@@ -3,5 +3,13 @@ function HandleOnClick() {
 }
 
 function UpdateTextLocation() {
+    const firstNameText = document.getElementById("first-name-text");
+    const surnameText = document.getElementById("surname-text");
+
+    const firstNameLoaction = document.getElementById("first-name-location");
+    const surnameLocation = document.getElementById("surname-location");
+
+    firstNameLoaction.innerText = firstNameText.value;
+    surnameLocation.innerText = surnameText.value;
 
 }
