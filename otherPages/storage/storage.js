31fe7d853ae523022c959acd550c89a2985ff51c
JPHumphreys <Jonathan.Humphreys@academytrainee.com>
2020-10-02 19:24:22 +0100

    did session storage

--- otherPages/storage/storage.js
+++ otherPages/storage/storage.js
@@ -6,9 +6,37 @@ function UpdateTextLocation() {
     const firstNameText = document.getElementById('first-name-text');
     const surnameText = document.getElementById('surname-text');
 
-    const firstNameLocation = document.getElementById('first-name-location');
-    const surnameLocation = document.getElementById('surname-location');
+    SetFirstName(firstNameText.value);
+    SetSurname(surnameText.value);
 
-    firstNameLocation.innerText = firstNameText.value;
-    surnameLocation.innerText = surnameText.value;
+    StoreInSessionStorage(firstNameText.value, surnameText.value);
+}
+
+function SetFirstName(firstname) {
+    const firstNameLocation = document.getElementById('first-name-location');
+    firstNameLocation.innerText = firstname;
+}
+
+function SetSurname(surname) {
+    const surnameLocation = document.getElementById('surname-location');
+    surnameLocation.innerText = surname;
+}
+
+function StoreInSessionStorage(firstname, surname) {
+    sessionStorage.setItem('firstname', firstname);
+    sessionStorage.setItem('surname', surname);
+}
+
+function GetFromSessionStorage() {
+    const firstname = sessionStorage.getItem('firstname');
+    const surname = sessionStorage.getItem('surname');
+
+    if (firstname != null) {
+        SetFirstName(firstname);
+    }
+
+    if (surname != null) {
+        SetSurname(surname);
+    }
+
 }
