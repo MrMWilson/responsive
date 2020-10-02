263057923bd34b736fdbfe58e4862ccd2b8300dd
Mike Wilson <mrmwilson@live.co.uk>
2020-10-02 19:46:03 +0100

    cleaned up code

--- otherPages/storage/storage.js
+++ otherPages/storage/storage.js
@@ -6,10 +6,18 @@ function UpdateTextLocation() {
     const firstNameText = document.getElementById("first-name-text");
     const surnameText = document.getElementById("surname-text");
 
-    const firstNameLoaction = document.getElementById("first-name-location");
-    const surnameLocation = document.getElementById("surname-location");
+    SetFirstName(firstNameText.value);
 
-    firstNameLoaction.innerText = firstNameText.value;
-    surnameLocation.innerText = surnameText.value;
+    SetSurname(surnameText.value);
+}
+
+function SetFirstName(firstname) {
+    const firstNameLoaction = document.getElementById("first-name-location");
+    firstNameLoaction.innerText = firstname;
+}
+
+function SetSurname(surname) {
+    const surnameLocation = document.getElementById("surname-location");
+    surnameLocation.innerText = surname;
 
 }
