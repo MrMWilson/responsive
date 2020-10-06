03df5a738f0ebb85e82d0c5491368e0e236e4cfe
Mike Wilson <mrmwilson@live.co.uk>
2020-10-06 16:32:53 +0100

    changing colour in HTML for practice

--- index.js
+++
@@ -1,8 +0,0 @@
-function backgroundColour() {
-    let red = document.getElementById('redSlider').value;
-    let green = document.getElementById('greenSlider').value;
-    let blue = document.getElementById('blueSlider').value;
-    let colour = 'rgb(' + red + ',' + green + ',' + blue + ')';
-
-    document.slider.backgroundColour = colour;
-}
