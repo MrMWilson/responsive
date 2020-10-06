7dc32cbecfa6d3fb15897ccb35a39e01b66fa409
Mike Wilson <mrmwilson@live.co.uk>
2020-10-06 14:44:54 +0100

    added basic colour function

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-function BeepBoop() {
-  console.log('beep boop');
+function backgroundColour() {
+    let red = document.getElementById('redSlider').value;
+    let green = document.getElementById('greenSlider').value;
+    let blue = document.getElementById('blueSlider').value;
+    let colour = 'rgb(' + red + ',' + green + ',' + blue + ')';
+
+    document.slider.backgroundColour = colour;
 }
