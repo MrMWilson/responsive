a61bafccb500257994f5eeb782cae791061f14c3
Mike Wilson <mrmwilson@live.co.uk>
2020-10-06 17:37:43 +0100

    added text for each colour slider

--- index.js
+++ index.js
@@ -4,4 +4,19 @@ function ChangeSliderColour() {
     let blue = document.getElementById('blueSlider');
 
     red.style.background = "rgb(" + red.value + ",0,0)";
+    green.style.background = "rgb(0," + green.value + ",0)";
+    blue.style.background = "rgb(0,0," + blue.value + ")";
+
+    UpdateSliderInfo();
+
+}
+
+function UpdateSliderInfo() {
+    var redValue = document.getElementById('red-value');
+    var greenValue = document.getElementById('green-value');
+    var blueValue = document.getElementById('blue-value');
+
+    redValue.textContent = document.getElementById('redSlider').value + "/255";
+    greenValue.textContent = document.getElementById('greenSlider').value + "/255";
+    blueValue.textContent = document.getElementById('blueSlider').value + "/255";
 }
